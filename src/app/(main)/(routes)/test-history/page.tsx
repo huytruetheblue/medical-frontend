114e1e1b84aeab3c96910109c1fb03724bd56126
Đỗ Ngọc Huy <99071294+huytruetheblue@users.noreply.github.com>
2023-11-14 23:34:54 +0700

    commit

--- src/app/(main)/(routes)/test-history/page.tsx
+++ src/app/(main)/(routes)/test-history/page.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import { useAppSelector } from "@/reduxs/hooks";
-import InfoComponents from "@/components/InfoComponent";
 import TestComponents from "@/components/TestComponent";
 
 const TestHistoryPage = () => {
